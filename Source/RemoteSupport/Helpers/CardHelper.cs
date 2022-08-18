c07cbad37f763a2bf04ba98d9e339133219baf12
DOTZ <ong_michelle_sh@hotmail.com>
2022-08-18 01:00:02 -0700

    Update CardHelper.cs

--- Source/RemoteSupport/Helpers/CardHelper.cs
+++ Source/RemoteSupport/Helpers/CardHelper.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CardHelper.cs" company="Microsoft">
+// <copyright file="CardHelper.cs" company="Microsoft">
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 
@@ -586,6 +586,12 @@ namespace Microsoft.Teams.Apps.RemoteSupport.Helpers
                     break;
                 case Constants.NormalString:
                     value = localizer.GetString("NormalText");
+                    break;
+                case Constants.ProblemString:
+                    value = localizer.GetString("ProblemText");
+                    break;
+                case Constants.EnquiryString:
+                    value = localizer.GetString("EnquiryText");
                     break;
                 case Constants.AssignedString:
                     value = localizer.GetString("AssignedText");
