1a85c70b4dd25d152a8c073db44361a3f5e3fa8e
DOTZ <ong_michelle_sh@hotmail.com>
2022-08-19 15:38:59 +0800

    test

--- Source/RemoteSupport/Cards/TicketCard.cs
+++ Source/RemoteSupport/Cards/TicketCard.cs
@@ -263,9 +263,8 @@ namespace Microsoft.Teams.Apps.RemoteSupport.Cards
                 CardHelper.GetAdaptiveCardColumnSet(localizer.GetString("RequestNumberText"), $"#{ticketDetail.RowKey}", localizer),
                 CardHelper.GetAdaptiveCardColumnSet(localizer.GetString("RequestTypeText"), ticketDetail.RequestType, localizer),
                 CardHelper.GetAdaptiveCardColumnSet(localizer.GetString("TitleDisplayText"), ticketDetail.Title, localizer),
-                
-                //CardHelper.GetAdaptiveCardColumnSet(localizer.GetString("RequestCategoryText"), ticketDetail.Title, localizer),
-                
+                CardHelper.GetAdaptiveCardColumnSet(localizer.GetString("RequestCategoryText"), ticketDetail.Title, localizer),
+
             });
             dynamicElements.AddRange(ticketAdditionalFields);
             dynamicElements.Add(CardHelper.GetAdaptiveCardColumnSet(localizer.GetString("DescriptionText"), ticketDetail.Description, localizer));
