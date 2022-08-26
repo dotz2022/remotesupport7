fb4e729d20a981b9a1c55e360c2c9a6a9199454a
DOTZ <ong_michelle_sh@hotmail.com>
2022-08-26 11:59:34 +0800

    Update TicketHelper.cs

--- Source/RemoteSupport/Helpers/TicketHelper.cs
+++ Source/RemoteSupport/Helpers/TicketHelper.cs
@@ -108,7 +108,7 @@ namespace Microsoft.Teams.Apps.RemoteSupport.Helpers
             ticketDetail.SmeConversationId = null;
             ticketDetail.SmeTicketActivityId = null;
             ticketDetail.TicketStatus = (int)TicketState.Unassigned;
-            ticketDetail.Severity = (int)(TicketSeverity)Enum.Parse(typeof(TicketSeverity), ticketDetail.RequestType ?? TicketSeverity.NChat.ToString());
+            ticketDetail.Severity = (int)(TicketSeverity)Enum.Parse(typeof(TicketSeverity), ticketDetail.RequestType ?? TicketSeverity.Chat.ToString());
             ticketDetail.Cat = (int)(TicketCat)Enum.Parse(typeof(TicketCat), ticketDetail.CategoryType ?? TicketCat.Problem.ToString());
             ticketDetail.AdditionalProperties = CardHelper.ValidateAdditionalTicketDetails(ticketAdditionalDetails, turnContext.Activity.Timestamp.Value.Offset);
             ticketDetail.CardId = cardId;
